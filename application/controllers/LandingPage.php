145801fdb7025a3ccc16fe8bca8e389a128703b4
hanifm <hanifmuflihulanwar@gmail.com>
2021-03-26 17:06:29 +0700

    push lur

--- application/controllers/LandingPage.php
+++ application/controllers/LandingPage.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-Class Landingpage extends CI_Controller {
+Class LandingPage extends CI_Controller {
 
 	public function index()
 	{
